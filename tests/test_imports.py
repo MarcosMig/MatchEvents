b2c931db447f6e25acb181362785027d98a9dec8
MarcosMig <marcos.mig.cardoso@gmail.com>
2026-04-19 15:39:03 +0100

    Expand import tests for YOLO factory

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,4 +1,4 @@
-from match_events.detectors import StubDetector
+from match_events.detectors import StubDetector, YoloDetector, build_detector
 from match_events.io import VideoReader
 from match_events.pipeline import MatchEventsPipeline
 from match_events.tracking import StubTracker
@@ -6,6 +6,8 @@ from match_events.tracking import StubTracker
 
 def test_imports() -> None:
     assert StubDetector is not None
+    assert YoloDetector is not None
+    assert build_detector is not None
     assert StubTracker is not None
     assert VideoReader is not None
     assert MatchEventsPipeline is not None
