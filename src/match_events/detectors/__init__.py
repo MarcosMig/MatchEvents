03237922bcc2a1014df8ac8f9aa8270dd71c9729
MarcosMig <marcos.mig.cardoso@gmail.com>
2026-04-19 15:38:26 +0100

    Export YOLO detector and factory

--- src/match_events/detectors/__init__.py
+++ src/match_events/detectors/__init__.py
@@ -1,3 +1,11 @@
 from match_events.detectors.base import BaseDetector, Detection, StubDetector
+from match_events.detectors.factory import build_detector
+from match_events.detectors.yolo_adapter import YoloDetector
 
-__all__ = ["BaseDetector", "Detection", "StubDetector"]
+__all__ = [
+    "BaseDetector",
+    "Detection",
+    "StubDetector",
+    "YoloDetector",
+    "build_detector",
+]
